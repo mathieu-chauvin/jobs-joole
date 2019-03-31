9ccebbda437c10ab35d7d65815267cf9dba08da7
Mathieu Chauvin <chauvinmat@eisti.eu>
2019-03-31 21:58:22 +0200

    web interface for data vizualization done

--- joole/dashboard/static/dashboard/js/results.js
+++ joole/dashboard/static/dashboard/js/results.js
@@ -1,1 +1,36 @@
-// ----> YOUR CODE HERE <----
+var co = costData.find(i => i.year == 2017);
+var costs = [{'mois':'2017-01','cost':co['janvier']},
+{'mois':'2017-02','cost':co['fevrier']},
+{'mois':'2017-03','cost':co['mars']},
+{'mois':'2017-04','cost':co['avril']},
+{'mois':'2017-05','cost':co['mai']},
+{'mois':'2017-06','cost':co['juin']},
+{'mois':'2017-07','cost':co['juillet']},
+
+{'mois':'2017-08','cost':co['aout']},
+
+{'mois':'2017-09','cost':co['septembre']},
+{'mois':'2017-10','cost':co['octobre']},
+{'mois':'2017-11','cost':co['novembre']},
+
+{'mois':'2017-12','cost':co['decembre']},
+
+
+];   
+
+
+new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'costChart',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: 
+	 costs,
+  // The name of the data record attribute that contains x-values.
+  xkey: 'mois',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['cost'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['Watt']
+});
--- joole/dashboard/views.py
+++ joole/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 from .forms import ClientForm
 from .models import Conso_eur, Conso_watt
 
@@ -16,20 +17,47 @@ class ClientFormView(View):
             client_id = form.cleaned_data['client']
             return redirect('dashboard:results', client_id=client_id)
 
+# calcule la consommation moyenne sur plusieurs mois
+def moy(list_mois, conso):
+    v = 0;
+    i = 0;
+    
+    for m in list_mois:
+        v += conso[m]
+        i = i+1;
+
+    if i != 0:
+        return v/i
+    else:
+        return 0
+
 def results(request, client_id):
     conso_euro = []
     conso_watt = []
-    annual_costs = [0, 0]
-    is_elec_heating = True
+    annual_costs = {}
+    json_conso_watt=""
+    is_elec_heating = False
     dysfunction_detected = False
 
-    ###################################
-    # ----> YOUR CODE GOES HERE <---- #
-    ###################################
+    if (client_id != 'login.html'):
+        conso_euro = Conso_eur.objects.filter(client_id = client_id)
+        for c in conso_euro: 
+            annual_costs[c.year] = c.janvier +  c.fevrier + c.mars + c.avril + c.mai + c.juin + c.aout + c.septembre + c.octobre + c.novembre + c.decembre
+
+        conso_watt = Conso_watt.objects.filter(client_id = client_id).values()
+        json_conso_watt = json.dumps(list(conso_watt), cls=DjangoJSONEncoder)
+
+        # si la consommation en hiver dépasse largement celle d'été
+        if (moy(['janvier','fevrier','decembre'],conso_watt[0]) > 2*moy(['juin','juillet','aout'],conso_watt[0])):
+            is_elec_heating =True 
+
+        # si la différence entre les deux années dépasse le seuil fixé (10%)
+        if (abs(annual_costs[2017] - annual_costs[2016])/annual_costs[2016] > 0.1):
+            dysfunction_detected = True
 
     context = {
         "conso_euro": conso_euro,
-        "conso_watt": conso_watt,
+        "json_conso_watt": json_conso_watt,
         "annual_costs": annual_costs,
         "is_elec_heating": is_elec_heating,
         "dysfunction_detected": dysfunction_detected
